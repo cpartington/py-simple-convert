cd34e5a760872d466b7507c37b597927357efd67
Christie Partington <christiedpartington@gmail.com>
2019-06-04 11:41:31 -0400

    Add convert functions

---
+++ convert/converter.py
@@ -0,0 +1,27 @@
+class Converter:
+    def __init__(self):
+        pass
+
+    def decimal_to_base(self, num, base=2):
+        """
+        Converts a decimal number to a number in the indicated number system.
+        The default is base two. Assumes that the provided number is a valid
+        integer in base 10.
+
+        :param num: the integer to be converted
+        :param base: the number system to convert to
+
+        :return: the converted number
+        """
+        pass
+
+    def base_to_decimal(self, num, base):
+        """
+        Converts a number in any base to a number in base 10 (decimal). Assumes
+        that the provided number is valid.
+
+        :param num: the number to be converted
+        :param base: the base of the provided number
+        :return:
+        """
+        pass
